725fe295c744bdfc69f74eb1f7d3f33cc4d93f9e
Erik Igelström <eigelstrom@gmail.com>
2019-12-24 22:37:10 +0000

    Initial commit

---
+++ lib/cube_puzzle.ex
@@ -0,0 +1,18 @@
+defmodule CubePuzzle do
+  @moduledoc """
+  Documentation for CubePuzzle.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CubePuzzle.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/cube_puzzle_test.exs
@@ -0,0 +1,8 @@
+defmodule CubePuzzleTest do
+  use ExUnit.Case
+  doctest CubePuzzle
+
+  test "greets the world" do
+    assert CubePuzzle.hello() == :world
+  end
+end
