eee454c1cf60889b2b9e75575b144998574fc888
Erik Igelström <eigelstrom@gmail.com>
2019-12-26 13:54:32 +0000

    Walk 1+ steps in a line

--- lib/cube_puzzle.ex
+++ lib/cube_puzzle.ex
@@ -17,6 +17,40 @@ defmodule CubePuzzle do
   end
 
   @doc """
+    # Walks in a straight line
+    iex> CubePuzzle.walk([2])
+    [{0, 0, 0}, {1, 0, 0}, {2, 0, 0}]
+  """
+  def walk(moves) do
+    moves
+    |> List.foldl([{0, 0, 0}], fn move, acc -> step(acc, {1, 0, 0}, move) end)
+    |> Enum.reverse()
+  end
+
+  @doc """
+  Given a previous path and a direction, adds one or more new steps in the given direction.
+    iex> CubePuzzle.step([{0, 0, 0}], {1, 0, 0}, 1)
+    [{1, 0, 0}, {0, 0, 0}]
+
+    iex> CubePuzzle.step([{0, 0, 0}], {1, 0, 0}, 3)
+    [{3, 0, 0}, {2, 0, 0}, {1, 0, 0}, {0, 0, 0}]
+
+    iex> CubePuzzle.step([{1, 0, 0}, {0, 0, 0}], {0, -1, 0}, 1)
+    [{1, -1, 0}, {1, 0, 0}, {0, 0, 0}]
+  """
+  def step(path, _direction, 0), do: path
+  def step(path, direction, n) do
+    [last_position | _] = path
+    next_position = add_vector(last_position, direction)
+    step(
+      [next_position | path],
+      direction,
+      n - 1
+    )
+  end
+  defp add_vector({a, b, c}, {d, e, f}), do: {a + d, b + e, c + f}
+
+  @doc """
   Takes a direction vector and an arbitrary number in 0..3.
   Turns in one of the four possible directions and returns a new direction vector.
 
