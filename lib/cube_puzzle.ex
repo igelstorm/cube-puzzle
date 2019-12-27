92e46c1471d1b8774d75b9024471b97352e1f2fc
Erik Igelström <eigelstrom@gmail.com>
2019-12-27 12:02:04 +0000

    Move doctests into test file

--- lib/cube_puzzle.ex
+++ lib/cube_puzzle.ex
@@ -1,17 +1,4 @@
 defmodule CubePuzzle do
-  @moduledoc """
-  Documentation for CubePuzzle.
-  """
-
-  @doc """
-    # Can walk in a straight line
-    iex> CubePuzzle.walk([2])
-    [{0, 0, 0}, {1, 0, 0}, {2, 0, 0}]
-
-    # Can turn between each move
-    iex> CubePuzzle.walk([1, 1])
-    [{0, 0, 0}, {1, 0, 0}, {1, -1, 0}]
-  """
   def walk(moves) do
     directions = [{1, 0, 0}, {0, -1, 0}]
     moves
@@ -20,17 +7,6 @@ defmodule CubePuzzle do
     |> Enum.reverse()
   end
 
-  @doc """
-  Given a previous path and a direction, adds one or more new steps in the given direction.
-    iex> CubePuzzle.step({1, {1, 0, 0}}, [{0, 0, 0}])
-    [{1, 0, 0}, {0, 0, 0}]
-
-    iex> CubePuzzle.step({3, {1, 0, 0}}, [{0, 0, 0}])
-    [{3, 0, 0}, {2, 0, 0}, {1, 0, 0}, {0, 0, 0}]
-
-    iex> CubePuzzle.step({1, {0, -1, 0}}, [{1, 0, 0}, {0, 0, 0}])
-    [{1, -1, 0}, {1, 0, 0}, {0, 0, 0}]
-  """
   def step({0, _}, path), do: path
   def step({n, direction}, path) do
     [last_position | _] = path
@@ -42,19 +18,6 @@ defmodule CubePuzzle do
   end
   defp add_vector({a, b, c}, {d, e, f}), do: {a + d, b + e, c + f}
 
-  @doc """
-  Takes a direction vector and an arbitrary number in 0..3.
-  Turns in one of the four possible directions and returns a new direction vector.
-
-    iex> CubePuzzle.turn({1, 0, 0}, 0)
-    {0, 1, 0}
-
-    iex> CubePuzzle.turn({-1, 0, 0}, 1)
-    {0, -1, 0}
-
-    iex> CubePuzzle.turn({0, 0, 1}, 3)
-    {0, -1, 0}
-  """
   def turn({a, 0, 0}, 0) when a != 0, do: {0, 1, 0}
   def turn({a, 0, 0}, 1) when a != 0, do: {0, -1, 0}
   def turn({a, 0, 0}, 2) when a != 0, do: {0, 0, 1}
--- test/cube_puzzle_test.exs
+++ test/cube_puzzle_test.exs
@@ -1,4 +1,34 @@
 defmodule CubePuzzleTest do
   use ExUnit.Case
   doctest CubePuzzle
+
+  # walk/1
+  test "walks in a straight line" do
+    assert CubePuzzle.walk([2]) == [{0, 0, 0}, {1, 0, 0}, {2, 0, 0}]
+  end
+
+  test "turns between each move" do
+    assert CubePuzzle.walk([1, 1]) == [{0, 0, 0}, {1, 0, 0}, {1, -1, 0}]
+  end
+
+  # step/2
+  test "can add one new steps in a given direction to an existing path" do
+    assert CubePuzzle.step({1, {1, 0, 0}}, [{0, 0, 0}]) == [{1, 0, 0}, {0, 0, 0}]
+  end
+
+  test "can add several steps in the same direction" do
+    assert CubePuzzle.step({3, {1, 0, 0}}, [{0, 0, 0}]) == [{3, 0, 0}, {2, 0, 0}, {1, 0, 0}, {0, 0, 0}]
+  end
+
+  test "can add a step in a different direction" do
+    assert CubePuzzle.step({1, {0, -1, 0}}, [{1, 0, 0}, {0, 0, 0}]) == [{1, -1, 0}, {1, 0, 0}, {0, 0, 0}]
+  end
+
+  # turn/2
+  test "takes a direction vector and 'turns' it into another direction, based on a number" do
+    assert CubePuzzle.turn({1, 0, 0}, 0)  == {0, 1, 0}
+    assert CubePuzzle.turn({-1, 0, 0}, 1) == {0, -1, 0}
+    assert CubePuzzle.turn({0, 0, 1}, 3)  == {0, -1, 0}
+  end
+
 end
