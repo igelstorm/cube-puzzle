3c8ece62e183062f9c5995c80a4eaf76fe4b857b
Erik Igelström <eigelstrom@gmail.com>
2019-12-27 22:08:33 +0000

    Identify whether a list of coordinates is a cube

--- lib/cube_puzzle.ex
+++ lib/cube_puzzle.ex
@@ -1,6 +1,35 @@
 defmodule CubePuzzle do
   @starting_position {0, 0, 0}
   @the_puzzle [2, 1, 1, 2, 1, 2, 1, 1, 2, 2, 1, 1, 1, 2, 2, 2, 2]
+  @cube [
+      {0, 0, 0},
+      {0, 0, 1},
+      {0, 0, 2},
+      {0, 1, 0},
+      {0, 1, 1},
+      {0, 1, 2},
+      {0, 2, 0},
+      {0, 2, 1},
+      {0, 2, 2},
+      {1, 0, 0},
+      {1, 0, 1},
+      {1, 0, 2},
+      {1, 1, 0},
+      {1, 1, 1},
+      {1, 1, 2},
+      {1, 2, 0},
+      {1, 2, 1},
+      {1, 2, 2},
+      {2, 0, 0},
+      {2, 0, 1},
+      {2, 0, 2},
+      {2, 1, 0},
+      {2, 1, 1},
+      {2, 1, 2},
+      {2, 2, 0},
+      {2, 2, 1},
+      {2, 2, 2}
+    ]
 
   def solve_the_puzzle do
     fold_puzzle(@the_puzzle, random_directions())
@@ -32,4 +61,19 @@ defmodule CubePuzzle do
   end
 
   defp add_vector({a, b, c}, {d, e, f}), do: {a + d, b + e, c + f}
+
+  def cube?(coordinates) do
+    coordinates
+    |> normalize()
+    |> Enum.sort() == @cube
+  end
+
+  defp normalize(coordinates) do
+    {min_x, _, _} = Enum.min_by(coordinates, fn {x, _, _} -> x end)
+    {_, min_y, _} = Enum.min_by(coordinates, fn {_, y, _} -> y end)
+    {_, _, min_z} = Enum.min_by(coordinates, fn {_, _, z} -> z end)
+    
+    coordinates
+    |> Enum.map(fn coord -> add_vector(coord, {-min_x, -min_y, -min_z}) end)
+  end
 end
